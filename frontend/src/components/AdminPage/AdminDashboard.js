1909bc7ac025e11049f7e35bab50e1c817e59eb1
Hans-Axle <consuelohansaxle07@gmail.com>
2025-04-07 21:57:29 +0800

    Update AdminDashboard.js
    admin bootstrap

--- frontend/src/components/AdminPage/AdminDashboard.js
+++ frontend/src/components/AdminPage/AdminDashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import AdminProductsTable from "./AdminProductsTable";
 import OrdersTable from "./OrdersTable";
 import axios from "axios";
