a0733635159dba62e675277f6283abc74f90a24b
Hans-Axle <consuelohansaxle07@gmail.com>
2025-04-07 22:02:05 +0800

    Update ProductCatalog.js

--- frontend/src/components/CustomerPage/ProductCatalog.js
+++ frontend/src/components/CustomerPage/ProductCatalog.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Modal, Button, Form, Dropdown } from "react-bootstrap";
 import axios from "axios";
 import Login from "../DefaultPage/Login"; // Import Login component
 
